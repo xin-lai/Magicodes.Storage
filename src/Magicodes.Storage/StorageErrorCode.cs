54e7b8064e1a91726f624fa8fda8dba9c202e800
李文强 <codelove1314@live.cn>
2020-02-06 17:23:18 +0800

    修改目录

---
+++ src/Magicodes.Storage/StorageErrorCode.cs
@@ -0,0 +1,30 @@
+﻿// ======================================================================
+//  
+//          Copyright (C) 2016-2020 湖南心莱信息科技有限公司    
+//          All rights reserved
+//  
+//          filename : StorageErrorCode.cs
+//          description :
+//  
+//          created by 李文强 at  2016/09/23 9:41
+//          Blog：http://www.cnblogs.com/codelove/
+//          GitHub ： https://github.com/xin-lai
+//          Home：http://xin-lai.com
+//  
+// ======================================================================
+
+namespace Magicodes.Storage
+{
+    public enum StorageErrorCode
+    {
+        None = 0,
+        InvalidCredentials = 1000,
+        GenericException = 1001,
+        InvalidAccess = 1002,
+        BlobInUse = 1003,
+        InvalidBlobName = 1004,
+        InvalidContainerName = 1005,
+        ErrorOpeningBlob = 1006,
+        NoCredentialsProvided = 1007
+    }
+}
---
+++ src/Magicodes.Storage/StorageException.cs
@@ -0,0 +1,32 @@
+﻿// ======================================================================
+//  
+//          Copyright (C) 2016-2020 湖南心莱信息科技有限公司    
+//          All rights reserved
+//  
+//          filename : StorageException.cs
+//          description :
+//  
+//          created by 李文强 at  2016/09/23 9:41
+//          Blog：http://www.cnblogs.com/codelove/
+//          GitHub ： https://github.com/xin-lai
+//          Home：http://xin-lai.com
+//  
+// ======================================================================
+
+using System;
+
+namespace Magicodes.Storage
+{
+    public class StorageException : Exception
+    {
+        public StorageException(StorageError error, Exception ex) : base(error.Message, ex)
+        {
+            ErrorCode = error.Code;
+            ProviderMessage = ex?.Message;
+        }
+
+        public int ErrorCode { get; private set; }
+
+        public string ProviderMessage { get; set; }
+    }
+}
